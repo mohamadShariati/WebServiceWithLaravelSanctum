6e8b82dba9dcfc775b80baad5e328c0bdb039170
mohamadshariati <mohamadshariati0996@gmail.com>
2022-11-12 09:12:34 +0330

    login and logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -38,4 +38,45 @@ class AuthController extends Controller
             'token'=>$token
         ],200);
     }
+
+    public function login(Request $request)
+    {
+        $validator=Validator::make($request->all(),[
+            'email'=>'required|email',
+            'password'=>'required'
+        ]);
+
+        if($validator->fails())
+        {
+            return response()->json($validator->messages(),422);
+        }
+
+        $user = User::where('email',$request->email)->first();
+
+        if(!$user)
+        {
+            return response()->json('user not found!!!',404);
+        }
+
+        if(!Hash::check($request->password, $user->password))
+        {
+            return response()->json('password is incorct!!!',422);
+        }
+
+        $token=$user->createToken('myToken')->plainTextToken;
+
+        return response()->json([
+            'user'=>$user,
+            'token'=>$token
+        ]);
+    }
+
+    public function logout()
+    {
+        auth()->user()->tokens->each(function($token,$key){
+            $token->delete();
+        });
+
+        return response()->json('Loged out successfully!!!',200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +22,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::post('/register',[AuthController::class,'register']);
+Route::post('/login',[AuthController::class,'login']);
+Route::post('/logout',[AuthController::class,'logout'])->middleware('auth:sanctum');
+
+
+Route::get('/users',[UserController::class,'index'])->middleware('auth:sanctum');
